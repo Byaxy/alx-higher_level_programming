7b47ca7e9cfa59bc0e6604465a84271ce172d8f2
Byaxy <charlesbyaxy@gmail.com>
2021-12-14 10:11:13 -0800

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let argmnt = process.argv.length;
+if (argmnt === 2) {
+  console.log('No argument');
+} else if (argmnt === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
